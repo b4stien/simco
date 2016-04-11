00769ffb6bf2f4f9c47cdfd04f9bbc5c5949a422
Bastien Gandouet <bastien@mozaiqu.es>
2016-04-11 11:28:07 +0200

    Initial +2 commit

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+webpackJsonp([1],{
+
+/***/ 204:
+/***/ function(module, exports) {
+
+	"use strict";
+
+/***/ }
+
+},[204]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+var path = require('path'),
+    webpack = require('webpack');
+
+
+var rootdir = path.resolve(__dirname);
+
+
+var webpackConfig = {
+  context: __dirname + '/js',
+  entry: {
+    'app': './main',
+    'vendor': [
+      'babel-polyfill',
+      'immutable',
+      'whatwg-fetch',
+      'react',
+      'react-dom',
+      'redux',
+      'react-redux'
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: [/simco\/js/],
+        query: {
+          presets: ['es2015-webpack', 'react', 'stage-2']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js' })
+  ],
+  resolve: {
+    modules: ['../node_modules'],
+    extensions: ['', '.jsx', '.js', '.json'],
+    alias: {
+      simco: path.resolve(__dirname, 'js')
+    }
+  },
+  output: {
+    publicPath: '/dist/',
+    path: path.resolve(rootdir, 'dist'),
+    filename: '[name].js'
+  },
+  debug: true
+};
+
+
+
+if(process.argv.indexOf('-p') >= 0) {
+  webpackConfig['devtool'] = 'source-map';
+  webpackConfig.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {'NODE_ENV': JSON.stringify('production')}
+    })
+  );
+}
+
+else {
+  webpackConfig['devtool'] = 'inline-source-map';
+}
+
+module.exports = webpackConfig;
